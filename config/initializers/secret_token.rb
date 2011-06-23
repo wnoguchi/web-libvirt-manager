d1a00631206416e09520d027275d509c70ee8f2e
Wataru Noguchi <noguchi@madoka.(none)>
2011-06-23 11:23:19 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebLibvirtManager::Application.config.secret_token = 'd77f9ecf7fa50bc2634a938bb61c6c56e0ef0c51afdc4f3af03fb9cda136d64045b098ccafabb8b0b8f417b29c74b35d73eb1310e412dd62fce3e14fa132934a'
