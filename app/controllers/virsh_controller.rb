0ecb715e7e74ffcde812c787461c4bd07b00747b
Wataru Noguchi <wnoguchi@u-cast.com>
2011-06-23 15:08:44 +0900

    cut header

--- app/controllers/virsh_controller.rb
+++ app/controllers/virsh_controller.rb
@@ -1,7 +1,12 @@
 class VirshController < ApplicationController
   def index
-    @result = `sudo virsh list --all`
-#    @result = `id`
+    @domain_list_str = `sudo virsh list --all`
+    @count = -2
+    @domain_list = []
+    @domain_list_str.each_line do |line|
+      @count += 1
+      next if @count <= 0
+      @domain_list << line.chomp
+    end
   end
-
 end
